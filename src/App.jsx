6516462844e7a822e89736ec3a3a1d0e4dd349af
Korey Nichols <koreynichols1@gmail.com>
2026-04-21 15:38:39 -0700

    uncommented TodoFrom import and instance in return

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { useState } from 'react';
 
 import TodoList from './TodoList.jsx';
-// import TodoForm from './TodoForm.jsx';
+import TodoForm from './TodoForm.jsx';
 
 
 const todos = [
@@ -20,7 +20,7 @@ function App() {
   return (
       <div>
         <h1>Todo List</h1>
-        {/* <TodoForm></TodoForm> */}
+        <TodoForm></TodoForm>
         <TodoList todoList={todoList}></TodoList>    
       </div>
   )
