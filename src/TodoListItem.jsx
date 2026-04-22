c43e547701764a60886cfd966f8c6cb77b2a203b
Korey Nichols <koreynichols1@gmail.com>
2026-04-21 20:44:49 -0700

    Changed TodoListItem to use object destructuring to access todo

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,7 +1,7 @@
-function TodoListItem(props) {
+function TodoListItem({todo}) {
     return(
         <>
-            <li>{props.todo.title}</li>
+            <li>{todo.title}</li>
         </>
     )
 }
