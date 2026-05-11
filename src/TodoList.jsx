332e9fa8575ad2c833a84035b36764882a399f9e
Korey Nichols <koreynichols1@gmail.com>
2026-05-11 14:23:43 -0700

    added conditional render for empty list

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,18 @@
 import TodoListItem from "./TodoListItem";
 
 function TodoList({todoList}) {
+    console.log(todoList)
     return (
-        <ul>
-            {todoList.map(todo => <TodoListItem key={todo.id} todo={todo}></TodoListItem>)}
-        </ul>
+        <div>
+            {todoList.length == 0 ? (
+                <p>Add todo above to get started</p>
+            ) : (
+                <ul>
+                    {todoList.map(todo => <TodoListItem key={todo.id} todo={todo}></TodoListItem>)}
+                </ul>
+            )}
+        </div>
+        
     )
 }
 
